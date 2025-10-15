913d57c9cab511eadd0f7471711a9dccaa4caf60
Guilherme Estevão <guilhermewilliann@hotmail.com>
2025-10-14 21:27:31 -0300

    feat: aula 14/10 parte 2

--- 131025/adicionar_lista.py
+++ 131025/adicionar_lista.py
@@ -11,6 +11,8 @@ while True:
     print("Bem vindo a lista de contatos")
     print("1 - Cadastro novo contato")
     print("2 - Listar os contatos")
+    print("3 - Alterar contato")
+    print("4 - Remover contato")
     print("0 - Sair")
     opcao = input("Digite sua opção: ")
     if(opcao == "0"):
@@ -26,6 +28,30 @@ while True:
         for cont in contatos:
             print(f'Nome {cont[0]} | Telefone {cont[1]} | Cidade {cont[2]}')
         print('--------------------------------\n\n')
+    elif(opcao == "3"):
+        for indice in range(len(contatos)):
+            print(f'Código {indice} - Nome {contatos[indice][0]}')
+
+        indice = int(input('Digite o indice que deseja alterar: '))
+        while indice < 0 or indice >= len(contatos):
+            print('Inválido')
+            indice = int(input('Digite o indice que deseja alterar: '))
+
+        nome = input("Digite o novo nome do contato: ")
+        celular = input('Digite o novo seu celular: ')
+        cidade = input('Digite a nova cidade: ')
+        novaSublista = [nome, celular, cidade]
+        contatos[indice] = novaSublista
+    elif(opcao == "4"):
+        for indice in range(len(contatos)):
+            print(f'Código {indice} - Nome {contatos[indice][0]}')
+
+        indice = int(input('Digite o indice que deseja remover: '))
+        while indice < 0 or indice >= len(contatos):
+            print('Inválido')
+            indice = int(input('Digite o indice que deseja remover: '))
+
+        contatos.remove(contatos[indice])
     else:
         print('Erro, escolha uma opção correta')
 
